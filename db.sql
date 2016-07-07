96a55ccb42ef90c29c67f2f1c566e161a0f7aee6
JustinGrahamDev <justingraham@knights.ucf.edu>
2016-07-07 19:06:54 -0400

    added real db

--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 07, 2016 at 11:06 PM
+-- Generation Time: Jul 08, 2016 at 01:02 AM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -27,7 +27,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `admin` (
-  `sid` char(11) NOT NULL DEFAULT '',
+  `sid` int(11) NOT NULL,
   `name` char(30) DEFAULT NULL,
   `password` char(20) DEFAULT NULL,
   `email` char(30) DEFAULT NULL,
@@ -41,10 +41,10 @@ CREATE TABLE IF NOT EXISTS `admin` (
 --
 
 CREATE TABLE IF NOT EXISTS `admin_creates_event` (
-  `sid` char(11) NOT NULL DEFAULT '',
-  `time` time DEFAULT NULL,
-  `name` char(30) DEFAULT NULL,
-  PRIMARY KEY (`sid`),
+  `name` char(30) NOT NULL DEFAULT '',
+  `time` time NOT NULL DEFAULT '00:00:00',
+  `sid` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`sid`,`time`,`name`),
   KEY `time` (`time`),
   KEY `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -56,14 +56,26 @@ CREATE TABLE IF NOT EXISTS `admin_creates_event` (
 --
 
 CREATE TABLE IF NOT EXISTS `comments` (
-  `sid` char(11) NOT NULL DEFAULT '',
+  `sid` int(11) NOT NULL DEFAULT '0',
   `time` time NOT NULL DEFAULT '00:00:00',
   `name` char(30) NOT NULL DEFAULT '',
-  `text` char(100) DEFAULT NULL,
-  `timestamp` timestamp NULL DEFAULT NULL,
+  `timestamps` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`sid`,`time`,`name`),
   KEY `time` (`time`),
   KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `createsrso`
+--
+
+CREATE TABLE IF NOT EXISTS `createsrso` (
+  `rid` int(11) NOT NULL DEFAULT '0',
+  `sid` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`sid`,`rid`),
+  KEY `rid` (`rid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -74,7 +86,7 @@ CREATE TABLE IF NOT EXISTS `comments` (
 
 CREATE TABLE IF NOT EXISTS `create_profile` (
   `name` char(30) NOT NULL DEFAULT '',
-  `sid` char(11) DEFAULT NULL,
+  `sid` int(11) DEFAULT NULL,
   PRIMARY KEY (`name`),
   KEY `sid` (`sid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -119,12 +131,12 @@ CREATE TABLE IF NOT EXISTS `have_location` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `joins`
+-- Table structure for table `joinsrso`
 --
 
-CREATE TABLE IF NOT EXISTS `joins` (
-  `sid` char(11) NOT NULL DEFAULT '',
-  `rid` char(11) NOT NULL DEFAULT '',
+CREATE TABLE IF NOT EXISTS `joinsrso` (
+  `rid` int(11) NOT NULL DEFAULT '0',
+  `sid` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`sid`,`rid`),
   KEY `rid` (`rid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -145,14 +157,14 @@ CREATE TABLE IF NOT EXISTS `location` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `owns`
+-- Table structure for table `ownsrso`
 --
 
-CREATE TABLE IF NOT EXISTS `owns` (
-  `sid` char(11) DEFAULT NULL,
-  `rid` char(11) NOT NULL DEFAULT '',
-  PRIMARY KEY (`rid`),
-  KEY `sid` (`sid`)
+CREATE TABLE IF NOT EXISTS `ownsrso` (
+  `rid` int(11) NOT NULL DEFAULT '0',
+  `sid` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`sid`,`rid`),
+  KEY `rid` (`rid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -162,12 +174,13 @@ CREATE TABLE IF NOT EXISTS `owns` (
 --
 
 CREATE TABLE IF NOT EXISTS `rso` (
-  `rid` char(11) NOT NULL DEFAULT '',
+  `rid` int(11) NOT NULL AUTO_INCREMENT,
   `name` char(30) DEFAULT NULL,
+  `students` int(11) DEFAULT NULL,
   `university` char(30) DEFAULT NULL,
-  `description` char(100) DEFAULT NULL,
+  `description` char(50) DEFAULT NULL,
   PRIMARY KEY (`rid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -176,8 +189,8 @@ CREATE TABLE IF NOT EXISTS `rso` (
 --
 
 CREATE TABLE IF NOT EXISTS `rso_affil` (
-  `sid` char(11) NOT NULL DEFAULT '',
-  `rid` char(11) NOT NULL DEFAULT '',
+  `sid` int(11) NOT NULL DEFAULT '0',
+  `rid` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`sid`,`rid`),
   KEY `rid` (`rid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -189,12 +202,12 @@ CREATE TABLE IF NOT EXISTS `rso_affil` (
 --
 
 CREATE TABLE IF NOT EXISTS `student` (
-  `sid` char(11) NOT NULL DEFAULT '',
+  `sid` int(11) NOT NULL AUTO_INCREMENT,
   `name` char(30) DEFAULT NULL,
   `password` char(20) DEFAULT NULL,
   `email` varchar(30) NOT NULL,
   PRIMARY KEY (`sid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -203,25 +216,12 @@ CREATE TABLE IF NOT EXISTS `student` (
 --
 
 CREATE TABLE IF NOT EXISTS `student_creates_event` (
-  `sid` char(11) NOT NULL DEFAULT '',
-  `time` time NOT NULL DEFAULT '00:00:00',
   `name` char(30) NOT NULL DEFAULT '',
+  `time` time NOT NULL DEFAULT '00:00:00',
+  `sid` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`sid`,`time`,`name`),
   KEY `time` (`time`),
   KEY `name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `student_creates_rso`
---
-
-CREATE TABLE IF NOT EXISTS `student_creates_rso` (
-  `sid` char(11) NOT NULL DEFAULT '',
-  `rid` char(11) NOT NULL DEFAULT '',
-  PRIMARY KEY (`sid`,`rid`),
-  KEY `rid` (`rid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -231,12 +231,12 @@ CREATE TABLE IF NOT EXISTS `student_creates_rso` (
 --
 
 CREATE TABLE IF NOT EXISTS `super_admin` (
-  `sid` char(11) NOT NULL DEFAULT '',
+  `sid` int(11) NOT NULL AUTO_INCREMENT,
   `name` char(30) DEFAULT NULL,
   `password` char(20) DEFAULT NULL,
   `email` char(30) DEFAULT NULL,
   PRIMARY KEY (`sid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -256,12 +256,12 @@ CREATE TABLE IF NOT EXISTS `university` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `university_affil`
+-- Table structure for table `univ_affil`
 --
 
-CREATE TABLE IF NOT EXISTS `university_affil` (
-  `sid` char(11) NOT NULL DEFAULT '',
-  `name` char(30) NOT NULL,
+CREATE TABLE IF NOT EXISTS `univ_affil` (
+  `name` char(30) NOT NULL DEFAULT '',
+  `sid` int(11) DEFAULT NULL,
   PRIMARY KEY (`name`),
   KEY `sid` (`sid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -273,12 +273,12 @@ CREATE TABLE IF NOT EXISTS `university_affil` (
 --
 
 CREATE TABLE IF NOT EXISTS `user` (
-  `sid` char(11) NOT NULL,
+  `sid` int(11) NOT NULL AUTO_INCREMENT,
   `name` char(30) DEFAULT NULL,
   `password` char(20) DEFAULT NULL,
   `email` char(20) DEFAULT NULL,
   PRIMARY KEY (`sid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 --
 -- Constraints for dumped tables
@@ -288,8 +288,9 @@ CREATE TABLE IF NOT EXISTS `user` (
 -- Constraints for table `admin_creates_event`
 --
 ALTER TABLE `admin_creates_event`
-  ADD CONSTRAINT `admin_creates_event_ibfk_1` FOREIGN KEY (`time`) REFERENCES `event` (`time`),
-  ADD CONSTRAINT `admin_creates_event_ibfk_2` FOREIGN KEY (`name`) REFERENCES `location` (`name`);
+  ADD CONSTRAINT `admin_creates_event_ibfk_1` FOREIGN KEY (`sid`) REFERENCES `admin` (`sid`),
+  ADD CONSTRAINT `admin_creates_event_ibfk_2` FOREIGN KEY (`time`) REFERENCES `event` (`time`),
+  ADD CONSTRAINT `admin_creates_event_ibfk_3` FOREIGN KEY (`name`) REFERENCES `location` (`name`);
 
 --
 -- Constraints for table `comments`
@@ -300,23 +301,31 @@ ALTER TABLE `comments`
   ADD CONSTRAINT `comments_ibfk_3` FOREIGN KEY (`name`) REFERENCES `location` (`name`);
 
 --
+-- Constraints for table `createsrso`
+--
+ALTER TABLE `createsrso`
+  ADD CONSTRAINT `createsrso_ibfk_1` FOREIGN KEY (`sid`) REFERENCES `student` (`sid`),
+  ADD CONSTRAINT `createsrso_ibfk_2` FOREIGN KEY (`rid`) REFERENCES `rso` (`rid`);
+
+--
 -- Constraints for table `create_profile`
 --
 ALTER TABLE `create_profile`
   ADD CONSTRAINT `create_profile_ibfk_1` FOREIGN KEY (`sid`) REFERENCES `super_admin` (`sid`);
 
 --
--- Constraints for table `joins`
+-- Constraints for table `joinsrso`
 --
-ALTER TABLE `joins`
-  ADD CONSTRAINT `joins_ibfk_1` FOREIGN KEY (`sid`) REFERENCES `student` (`sid`),
-  ADD CONSTRAINT `joins_ibfk_2` FOREIGN KEY (`rid`) REFERENCES `rso` (`rid`);
+ALTER TABLE `joinsrso`
+  ADD CONSTRAINT `joinsrso_ibfk_1` FOREIGN KEY (`sid`) REFERENCES `student` (`sid`),
+  ADD CONSTRAINT `joinsrso_ibfk_2` FOREIGN KEY (`rid`) REFERENCES `rso` (`rid`);
 
 --
--- Constraints for table `owns`
+-- Constraints for table `ownsrso`
 --
-ALTER TABLE `owns`
-  ADD CONSTRAINT `owns_ibfk_1` FOREIGN KEY (`sid`) REFERENCES `admin` (`sid`);
+ALTER TABLE `ownsrso`
+  ADD CONSTRAINT `ownsrso_ibfk_1` FOREIGN KEY (`sid`) REFERENCES `student` (`sid`),
+  ADD CONSTRAINT `ownsrso_ibfk_2` FOREIGN KEY (`rid`) REFERENCES `rso` (`rid`);
 
 --
 -- Constraints for table `rso_affil`
@@ -334,20 +343,10 @@ ALTER TABLE `student_creates_event`
   ADD CONSTRAINT `student_creates_event_ibfk_3` FOREIGN KEY (`name`) REFERENCES `location` (`name`);
 
 --
--- Constraints for table `student_creates_rso`
+-- Constraints for table `univ_affil`
 --
-ALTER TABLE `student_creates_rso`
-  ADD CONSTRAINT `student_creates_rso_ibfk_1` FOREIGN KEY (`sid`) REFERENCES `student` (`sid`),
-  ADD CONSTRAINT `student_creates_rso_ibfk_2` FOREIGN KEY (`rid`) REFERENCES `rso` (`rid`);
-
---
--- Constraints for table `super_admin`
---
-ALTER TABLE `super_admin`
-  ADD CONSTRAINT `super_admin_ibfk_1` FOREIGN KEY (`sid`) REFERENCES `university` (`name`);
---
--- Database: `test`
---
+ALTER TABLE `univ_affil`
+  ADD CONSTRAINT `univ_affil_ibfk_1` FOREIGN KEY (`sid`) REFERENCES `student` (`sid`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
