f31dea4f84d5b64dc46917da8d84d82f1314c961
JustinGrahamDev <justingraham@knights.ucf.edu>
2016-07-08 18:30:40 -0400

    dash

--- dashboard.php
+++ dashboard.php
@@ -5,16 +5,20 @@
 <body>
 <?php include 'functions.php';
 	createUser("test", "test", "testemail");
+	
+	//get current user logged in
 	$user = getUser("test", "test");
-	echo $user->username;
 ?>
 	<div id="pageWrapper">
 		<div id="header">
 			<p id="siteTitle">Univent</p>
 			<p id="signifier">Welcome, <?php print $user->username ?></p>
 		</div>
-
-	<select>
+	
+	
+	
+	<select name="list" id="list" accesskey="target">
+		<option value='none' selected>Choose your destiny.</option>
 		<option value="viewAll">View All Events</option>
 		<option value="viewPublic">View Public Events</option>
 		<option value="viewPrivate">View Private Events</option>
@@ -24,12 +28,40 @@
 		<option value="createEvent">Create Event</option>
 		<option value="createUniv">Create University</option>
 	</select>
+	<input type=button value="Go" onclick="makeTheTable()"/>
+	
+	<h1>This is where the table goes</h1>
+	
+	<?php
+		function makeTheTable()
+		{
+			if($_POST['list']==viewAll)
+				$arrayOfData = getAllEvents($user);
+			if($_POST['list']==viewPublic)
+				$arrayOfData = getPublicEvents($user);
+			if($_POST['list']==viewPrivate)
+				$arrayOfData = getPrivateEvents($user);
+			if($_POST['list']==viewRSO)
+				$arrayOfData = getRSOevents($user);
+				
+			//createTable($arrayOfData);
+				
+			if($_POST['list']==joinRSO)
+				$didJoin = joinRSO($user);
+				//do confirmation here
+			if($_POST['list']==createRSO)
+				$arrayOfData = ($user);
+			if($_POST['list']==createEvent)
+				$arrayOfData = getAllEvents($user);
+			if($_POST['list']==createUniv)
+				$arrayOfData = getAllEvents($user);
+			
+		}
+		
+		
+	?>
 	
 	</div>
 </body>
 </html>
 
-//change views and functionality based off of dropdown select//
-//check privileges
-//create table of size 5; next and previous selectors
-//reload page
