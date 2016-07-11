d3d7ffb0fae36714b1c56036a1de58cf458a1642
JustinGrahamDev <justingraham@knights.ucf.edu>
2016-07-11 12:12:38 -0400

    dash update

--- dashboard.php
+++ dashboard.php
@@ -1,21 +1,23 @@
 <!DOCTYPE html>
 <html>
+
 <head>
+	<meta charset="utf-8">
 </head>
 <body>
 <?php include 'functions.php';
 	createUser("test", "test", "testemail");
-
+	
 	//get current user logged in
-	$user = stringToUser($_POST["user"]);
+	$user = getUser("test", "test");
 ?>
 	<div id="pageWrapper">
 		<div id="header">
 			<p id="siteTitle">Univent</p>
 			<p id="signifier">Welcome, <?php print $user->username ?></p>
 		</div>
-
-
+	
+	
 	<form name="menuOp" method="post" onsubmit="location.reload()">
 	<select name="list">
 		<option value='none' selected>Choose your destiny.</option>
@@ -30,40 +32,52 @@
 	</select>
 	<input type='submit' name='submit'\>
 	</form>
-
-
+	
+	
 	<h1>This is where the table goes</h1>
-
+	
 	<?php
-
-
-			if($_POST['list']=="viewAll")
+			
+			
+			if($_POST['list']=="viewAll"){
 				echo "test";
-				//$arrayOfData = getAllEvents($user);
+			}	//$arrayOfData = getAllEvents($user->sid);
 			if($_POST['list']=="viewPublic")
+				echo "test1";
 				//$arrayOfData = getPublicEvents($user);
 			if($_POST['list']=="viewPrivate")
+				echo "test2";
 				//$arrayOfData = getPrivateEvents($user);
 			if($_POST['list']=="viewRSO")
+				echo "test3";
 				//$arrayOfData = getRSOevents($user);
-
+				
 			//createTable($arrayOfData);
-
+				
 			if($_POST['list']=="joinRSO")
+				echo "test4";
 				//$didJoin = joinRSO($user);
 				//do confirmation here
 			if($_POST['list']=="createRSO")
+				echo "test5";
 				//new page for create rso redirect here
 			if($_POST['list']=="createEvent")
+				echo "<form action='createEvents.php' method='post' name='redirectCreateEvent'>";
 				//new page for create event redirect here
+				
+				
 			if($_POST['list']=="createUniv")
-				//new page for create univ redirect here
-
-
-
-
+				//$userlvl = getUserLevel($user->sid);
+				//if($userlvl==3)
+					//new page for create univ redirect here
+				//else echo "Credentials not acquired."
+			
+		
+		
+		
 	?>
-
+	
 	</div>
 </body>
 </html>
+
